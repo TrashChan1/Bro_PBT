d4a7547c2007b34ea22aeafc9cce9a9634046317
Ryan <rreddemann@cs.csub.edu>
2025-09-19 17:53:12 -0700

    changed the stack screen name for index from Home to Login

--- expo_frontend/app/_layout.tsx
+++ expo_frontend/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="index" options={{ title: 'Login' }} />
       <Stack.Screen name="newAccount" options={{ title: 'new_Account' }} />
     </Stack>
   );
