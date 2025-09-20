61c7ea632c0ee798f321f3769c7f2ac98fdb44ca
TrashChan1 <evenduregalendale@gmail.com>
2025-09-19 20:29:26 -0700

    Added user CRUD

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require("./sentry");
 require('./src/database/dbconfig/DBConnection');
+const mongoose = require('mongoose');
 const Utilities = require('./src/Utilities');
 const bodyParser = require('body-parser');
 const Sentry = require("@sentry/node");
@@ -12,6 +13,8 @@ const app = express();
 
 const { MongoClient } = require("mongodb");
 
+const CrudController = require("./src/controllers/CrudController");
+
 process.env.APP_DEBUG === 'true' && app.use(logger('dev'));
 app.use(cors(Utilities.corsOriginOptions));
 app.use(Utilities.corsError);
@@ -20,11 +23,20 @@ app.use(bodyParser.json({ limit: process.env.BODYPARSER_LIMIT, extended: true })
 app.use(bodyParser.urlencoded({ limit: process.env.BODYPARSER_LIMIT, extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 app.use(require(`./src/Routes`));
+app.use(express.json());
+app.use(cors());
 //app.use((req, res) => Utilities.apiResponse(res, 404, '404 API Not Found'));
 // TODO: Set up API so that we do not unsafely bypass 404 API not found
 Sentry.setupExpressErrorHandler(app);
 
+//appcrasher;
 
+/*
+mongoose.connect(config.mongoURI, {useNewUrlParser:true,
+                                   useUnifiedTopology: true })
+                .then(() => console.log('MongoDB Connected'))
+                .catch(err => console.log(err));
+                */
 async function myRead() {
     const uri = "mongodb://127.0.0.1:27017/";
     const client = new MongoClient(uri);
@@ -43,6 +55,9 @@ app.get('/test1',
         //res.send(`Harry's age is: ${harry_user.age}`);
         res.send(harry_user);
     })
+
+app.get(('/test2',
+    CrudController.getUsers));
 
 const server = app.listen(process.env.PORT, () => {
 	console.log(`********** Server is running on  http://localhost:${server.address().port}  **********`);
--- backend/src/database/dbconfig/DBConfig.js
+++ backend/src/database/dbconfig/DBConfig.js
@@ -2,4 +2,5 @@ require('dotenv').config();
 
 const ENV = {};
 ENV.MONGO_URI = process.env.MONGO_URI;
+//ENV.MONGO_URI = "mongodb://127.0.0.1:27017";
 module.exports = ENV;
